fae611e5380e8ea5b064bc187caef723c686e030
David <jekthor@gmail.com>
2017-08-12 14:21:48 -0500

    lista de beacons empty

---
+++ beaconsdemo/app/models/page.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Page = (function () {
+    function Page() {
+    }
+    return Page;
+}());
+exports.Page = Page;
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoicGFnZS5qcyIsInNvdXJjZVJvb3QiOiIiLCJzb3VyY2VzIjpbInBhZ2UudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6Ijs7QUFFQTtJQUFBO0lBTUEsQ0FBQztJQUFELFdBQUM7QUFBRCxDQUFDLEFBTkQsSUFNQztBQU5ZLG9CQUFJIiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IHsgSW1hZ2VTb3VyY2UgfSBmcm9tIFwiaW1hZ2Utc291cmNlXCI7XG5cbmV4cG9ydCBjbGFzcyBQYWdle1xuICAgIElkOiBudW1iZXI7XG4gICAgVGl0bGU6IFN0cmluZztcbiAgICBOYW1lOiBTdHJpbmc7XG4gICAgSW1hZ2U6IEltYWdlU291cmNlO1xuICAgIEltYWdlQmFzZTY0OiBTdHJpbmc7XG59Il19
--- beaconsdemo/app/views/beacons/beacon.component.ts
+++ beaconsdemo/app/views/beacons/beacon.component.ts
@@ -1,11 +1,13 @@
 import {Component} from "@angular/core";
 import {selector} from 'rxjs/operator/publish';
-
+import { Page } from "../../models/page";
+ 
 @Component({
     selector: "beacons",
     templateUrl: "views/beacons/beacon.component.html",
     styleUrls: ["views/beacons/beacon.component.css"]
 })
 export class BeaconComponent{
+    pages : Array<Page>;
     constructor(){}
 }
